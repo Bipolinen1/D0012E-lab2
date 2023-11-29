87d3cf07dee672621aed8ce47a7756f5f5357a27
willmerw <willmer@wohlen.se>
2023-11-29 10:54:38 +0100

    fixade incremental

--- incremental.py
+++ incremental.py
@@ -1,23 +1,15 @@
+def incremental(ls):
 
-
-def incremental2(ls):
-
-    t = [float('inf'),float('inf'),float('inf')]
+    nls = [float('inf'), float('inf'), float('inf')]
 
     for i in ls:
-        if i < t[0]:
-            t[1] = t[0]
-            t[0] = i
+        if i < nls[0]:
+            nls[1] = nls[0]
+            nls[0] = i
+        elif i < nls[1]:
+            nls[2] = nls[1]
+            nls[1] = i
+        elif i < nls[2]:
+            nls[2] = i
 
-        elif i < t[1]:
-            t[2] = t[1]
-            t[1] = i
-        elif i < t[2]:
-            t[2] = i
-    return t
-
-
-ls = [10, 5, 6, 4, 2, 5, 3]
-
-print(incremental2(ls))
-
+        return nls
